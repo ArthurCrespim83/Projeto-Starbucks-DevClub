eb8a9e45a539db27476efa56fc4d1c904319b57b
Arthur Crespim <arthurcrespim83@gmail.com>
2025-09-06 12:21:34 -0300

    Add files via upload

---
+++ DevClub_Starbucks/scripts.js
@@ -0,0 +1,28 @@
+/*let nome = "arthur"
+let sobrenome= "crespim"
+let idade = "18 anos"
+
+console.log(sobrenome)
+
+function faleMeuNome(){
+    console.log(nome)
+}
+
+faleMeuNome()
+faleMeuNome()
+faleMeuNome()
+*/
+
+let circulo = document.querySelector(".circulo") 
+
+function trocarCor(cor){
+    circulo.style.background = cor
+}
+
+
+let imagemCopo = document.querySelector(".imagem-copo")
+
+function trocarImagem(novaimagemCopo){
+    imagemCopo.src = novaimagemCopo;
+}
+
